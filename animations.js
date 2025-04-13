e21d6dc852ee0d4a9d80aa24abd8838ae015d332
JanMarkusK <146342688+JanMarkusK@users.noreply.github.com>
2025-04-13 20:00:18 +0300

    Algne muusika kasti animatsioon tehtud

---
+++ animations.js
@@ -0,0 +1,9 @@
+//Siis kui leht on laetud laseb teha seda "kasti avamise" animatsiooni
+document.addEventListener('DOMContentLoaded', () => {
+   const box = document.getElementById('box');
+   if (box) {
+     box.addEventListener('click', () => {
+       box.classList.toggle('open');
+     });
+   }
+ });
