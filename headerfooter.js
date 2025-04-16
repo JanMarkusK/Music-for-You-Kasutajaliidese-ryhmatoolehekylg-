3cd38eeb7be76115e3d028320da43573606a37d3
joosep5 <joss@tlu.ee>
2025-04-16 15:37:00 +0300

    header ja footer korras

--- headerfooter.js
+++ headerfooter.js
@@ -1,10 +1,24 @@
-function toggleFilters() {
-    const filtersSection = document.getElementById("filtersSection");
-    filtersSection.classList.toggle("active");
-  }
+document.addEventListener("DOMContentLoaded", () => {
+    const arrowButton = document.getElementById("arrow-button");
+    const topCircle = document.getElementById("top-circle");
+    const filters = document.getElementById("filters");
   
-  function showSignup() {
-    document.getElementById("signupSection").classList.add("active");
-    document.getElementById("filtersSection").classList.remove("active");
-  }
+    const arrowButtonFooter = document.getElementById("arrow-button-footer");
+    const bottomCircle = document.getElementById("bottom-circle");
+    const Sign_upFooter = document.getElementById("Sign_up-footer");
+  
+    // Ülemine nupp
+    arrowButton.addEventListener("click", () => {
+      arrowButton.classList.toggle("moved");
+      topCircle.classList.toggle("moved");
+      filters.classList.toggle("visible");
+    });
+  
+    // Alumine nupp
+    arrowButtonFooter.addEventListener("click", () => {
+      arrowButtonFooter.classList.toggle("moved");
+      bottomCircle.classList.toggle("moved");
+      Sign_upFooter.classList.toggle("visible");//sign_up footer on tegelt 
+    });
+  });
   
