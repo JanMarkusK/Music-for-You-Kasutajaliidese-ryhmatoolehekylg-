bf3874a825471fabc34f6f42b947f263c6684ef2
Joosep Madar <146342736+joosep5@users.noreply.github.com>
2025-04-29 11:46:55 +0300

    ruudu kustutamine

--- headerfooter.js
+++ headerfooter.js
@@ -1,19 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const arrowButton = document.getElementById("arrow-button");
     const topCircle = document.getElementById("top-circle");
-    const topCube = document.getElementById("top-cube");  
     const filters = document.getElementById("filters");
   
     const arrowButtonFooter = document.getElementById("arrow-button-footer");
     const bottomCircle = document.getElementById("bottom-circle");
-    const bottomCube = document.getElementById("bottom-cube");  
     const Sign_upFooter = document.getElementById("Sign_up-footer");
   
     // Ülemine nupp
     arrowButton.addEventListener("click", () => {
       arrowButton.classList.toggle("moved");
       topCircle.classList.toggle("moved");
-      topCube.classList.toggle("moved");  
       filters.classList.toggle("visible");
     });
   
@@ -21,7 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
     arrowButtonFooter.addEventListener("click", () => {
       arrowButtonFooter.classList.toggle("moved");
       bottomCircle.classList.toggle("moved");
-      bottomCube.classList.toggle("moved");  
       Sign_upFooter.classList.toggle("visible");
     });
   });
